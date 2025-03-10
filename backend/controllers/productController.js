930f32ee5401a5e7494a7a1f031838ce2a810652
mohammed boulmokh <tommyspecter11@gmail.com>
2025-03-10 07:03:22 +0100

    testing the routes with postman

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,71 +1,65 @@
-
 import { sql } from "../config/db.js";
 
 // get all products
 
 export const getProducts = (req, res) => {
-    try {
-        const products =  sql`SELECT * FROM products
+  try {
+    const products = sql`SELECT * FROM products
         order by createdat DESC`;
-        console.log("data fetched successfully");
-        res.status(200).json({success: true, data:products});
-    } catch (err) { 
-        console.error(err);
-    }
-
+    console.log("data fetched successfully");
+    res.status(200).json({ success: true, data: products });
+  } catch (err) {
+    console.error(err);
+  }
 };
 // get single product
 
 export const getProduct = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await sql`select * from products where id=${id}`
-        res.status(200).json({success: true, data:product});
-    } catch (err) {
-        console.log(err);
-        
-    }
+  try {
+    const { id } = req.params;
+    const product = await sql`select * from products where id=${id}`;
+    res.status(200).json({ success: true, data: product });
+  } catch (err) {
+    console.log(err);
+  }
 };
-
 
 //create product
 
 export const createProduct = async (req, res) => {
-    try {
-
-        const {name ,image , price} = req.body;
-        if(!name || !image || !price){
-            return res.status(400).json({success: false, message: "Please provide all fields"});
-        }
-        const product =  await sql`insert into products (name, image, price) values (${name}, ${image}, ${price}) returning *`;
-        res.status(200).json({success: true, data:product});
-    } catch (err) {
-        console.log(err);
-        
+  try {
+    const { name, image, price } = req.body;
+    if (!name || !image || !price) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please provide all fields" });
     }
+    const product =
+      await sql`insert into products (name, image, price) values (${name}, ${image}, ${price}) returning *`;
+    res.status(200).json({ success: true, data: product[0] });
+  } catch (err) {
+    console.log(err);
+  }
 };
 //update product
 
-export const updateProduct = (req, res) => {
-    try {
-        const { id } = req.params; 
-        const {name, image, price } = req.body;
-        const product = sql`update products set name=${name}, image=${image}, price=${price} where id=${id} returning *`;
-        res.status(200).json({success: true, data:product});
-    } catch (err) {
-        console.log(err);
-        
-    }
+export const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, image, price } = req.body;
+    const product = await sql`update products set name=${name}, image=${image}, price=${price} where id=${id} returning *`;
+    res.status(200).json({ success: true, data: product[0] });
+  } catch (err) {
+    console.log(err);
+  }
 };
 // delete product
-export const deleteProduct = (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = sql`delete from products where id=${id} returning *`;
-        res.status(200).json({success: true, data:product});
-        
-    } catch (err) {
-        console.log(err);
-        
-    }
+export const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await sql`delete from products where id=${id} returning *`;
+    res.status(200).json({ success: true, data: product[0] });
+  } catch (err) {
+    console.log(err);
+  }
 };
