7815f128cc1ad41aaaa8c0b643450a37d0d10290
mohammed boulmokh <tommyspecter11@gmail.com>
2025-03-16 18:13:41 +0100

    built the home page

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,42 @@
+
+
+import { Link } from "react-router-dom";
+import EditIcon from 'lucide-react';
+
+
+function ProductCard({ product }) {
+  return (
+    <div className="card bg-base-100 shadow-xl hover:shadow-2xl trasition-shadow duration-300">
+      {/* The product image. */}
+      <figure className="relative pt-[56.25%]">
+        <img
+          src={product.image}
+          alt={product.name}
+          className="absolute top-0 left-0 w-full h-full object-cover"
+        />
+      </figure>
+      {/* The product details. */}
+
+      <div className="card-body">
+        {/* The product name and price. */}
+        <h1 className="card-title tect-lg font-semibold">{product.name}</h1>
+        <p className="text-2xl font-bold text-primary">
+          ${Number(product.price).toFixed(2)}
+        </p>
+        <div className="card-actions justify-end mt-4">
+          <Link to = {`/product/${product.id}`} className = "btn btn-sm btn-info btn-outline">
+          
+          <EditIcon className = "size-5"/>
+          </Link>
+          <button>
+            <Trasch2Icon className = "size-5"/>
+        
+          </button>
+                
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
